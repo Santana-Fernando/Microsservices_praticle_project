926e4ea5b897dc73d7b342f5e1a5522c51c3aa36
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2026-03-25 12:11:05 -0300

    test: criando testes para o endpoint post

---
+++ Test/Usuario.post/UsuarioPostControllerTest.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using Usuario.Application;
+using Xunit;
+using Usuario.post.Controllers;
+
+namespace Test.Usuario.post;
+
+public class UsuarioPostControllerTest
+{
+    private readonly Mock<IUsuarioPostServices> _mockService;
+    private readonly UsuarioPost _controller;
+
+    public UsuarioPostControllerTest()
+    {
+        _mockService = new Mock<IUsuarioPostServices>();
+        _controller = new UsuarioPost(_mockService.Object);
+    }
+
+    [Fact]
+    public void Register_ReturnsOk_WhenSuccess()
+    {
+        var viewModel = new UsuariosViewModel();
+
+        _mockService.Setup(s => s.Register(It.IsAny<UsuariosViewModel>()))
+            .Returns(new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                ReasonPhrase = "Success"
+            });
+
+        var result = _controller.Register(viewModel);
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(200, okResult.StatusCode);
+    }
+
+    [Fact]
+    public void Register_ReturnsBadRequest_WhenBadRequest()
+    {
+        var viewModel = new UsuariosViewModel();
+
+        _mockService.Setup(s => s.Register(It.IsAny<UsuariosViewModel>()))
+            .Returns(new HttpResponseMessage(HttpStatusCode.BadRequest)
+            {
+                ReasonPhrase = "Invalid data"
+            });
+
+        var result = _controller.Register(viewModel);
+
+        var badRequest = Assert.IsType<ObjectResult>(result);
+        Assert.Equal(400, badRequest.StatusCode);
+    }
+
+    [Fact]
+    public void Register_ReturnsInternalServerError_WhenError()
+    {
+        var viewModel = new UsuariosViewModel();
+
+        _mockService.Setup(s => s.Register(It.IsAny<UsuariosViewModel>()))
+            .Returns(new HttpResponseMessage(HttpStatusCode.InternalServerError)
+            {
+                ReasonPhrase = "Error"
+            });
+
+        var result = _controller.Register(viewModel);
+
+        var error = Assert.IsType<ObjectResult>(result);
+        Assert.Equal(500, error.StatusCode);
+    }
+
+    [Fact]
+    public void Register_ReturnsUnauthorized_WhenException()
+    {
+        var viewModel = new UsuariosViewModel();
+
+        _mockService.Setup(s => s.Register(It.IsAny<UsuariosViewModel>()))
+            .Throws(new Exception());
+
+        var result = _controller.Register(viewModel);
+
+        Assert.IsType<UnauthorizedResult>(result);
+    }
+}
---
+++ Test/Usuario.post/UsuarioPostServicesTest.cs
@@ -0,0 +1,80 @@
+﻿using Xunit;
+using Moq;
+using System.Net;
+using AutoMapper;
+using Usuario.Application;
+using Usuario.Domain;
+using System;
+using System.Xml.Linq;
+
+namespace Test.Usuario.post;
+public class UsuarioPostServicesTests
+{
+    private readonly Mock<IUsuarioPost> _mockRepository;
+    private readonly Mock<IMapper> _mockMapper;
+    private readonly UsuarioPostServices _service;
+
+    public UsuarioPostServicesTests()
+    {
+        _mockRepository = new Mock<IUsuarioPost>();
+        _mockMapper = new Mock<IMapper>();
+        _service = new UsuarioPostServices(_mockRepository.Object, _mockMapper.Object);
+    }
+
+    [Fact]
+    public void Register_ReturnsBadRequest_WhenValidationFails()
+    {
+        var viewModel = new UsuariosViewModel();
+
+        var result = _service.Register(viewModel);
+
+        Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+    }
+
+    [Fact]
+    public void Register_ReturnsOk_WhenSuccess()
+    {
+        var viewModel = new UsuariosViewModel
+        {
+            name = "fernando",
+            email = "fernando@gmail.com",
+            password = "123456",
+            passwordConfirmation = "123456"
+        };
+
+        var usuario = new Usuarios();
+
+        _mockMapper
+            .Setup(m => m.Map<Usuarios>(It.IsAny<UsuariosViewModel>()))
+            .Returns(usuario);
+
+        _mockRepository
+            .Setup(r => r.Register(It.IsAny<Usuarios>()))
+            .Verifiable();
+
+        var result = _service.Register(viewModel);
+
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        _mockRepository.Verify(r => r.Register(It.IsAny<Usuarios>()), Times.Once);
+    }
+
+    [Fact]
+    public void Register_ReturnsInternalServerError_WhenExceptionOccurs()
+    {
+        var viewModel = new UsuariosViewModel
+        {
+            name = "fernando",
+            email = "fernando@gmail.com",
+            password = "123456",
+            passwordConfirmation = "123456"
+        };
+
+        _mockMapper
+            .Setup(m => m.Map<Usuarios>(It.IsAny<UsuariosViewModel>()))
+            .Throws(new Exception("Erro inesperado"));
+
+        var result = _service.Register(viewModel);
+
+        Assert.Equal(HttpStatusCode.InternalServerError, result.StatusCode);
+    }
+}
