871c50b59eb388cd59a9947754ee94b485fcf0d5
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2026-03-26 18:12:05 -0300

    test: criando teste para endpoint de delete

---
+++ Test/Usuario.del/UsuarioDelControllerTests.cs
@@ -0,0 +1,76 @@
+﻿using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Usuario.Application;
+using System.Net.Http;
+using System;
+using Usuario.del.Controllers;
+
+namespace Test.Usuario.del;
+public class UsuarioDelTests
+{
+    private readonly Mock<IUsuarioDelService> _serviceMock;
+    private readonly UsuarioDel _controller;
+
+    public UsuarioDelTests()
+    {
+        _serviceMock = new Mock<IUsuarioDelService>();
+        _controller = new UsuarioDel(_serviceMock.Object);
+    }
+
+    [Fact]
+    public void Remove_DeveRetornar200_QuandoSucesso()
+    {
+        // Arrange
+        var response = new HttpResponseMessage(HttpStatusCode.OK);
+
+        _serviceMock
+            .Setup(s => s.Remove(It.IsAny<int>()))
+            .Returns(response);
+
+        // Act
+        var result = _controller.Remove(1);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(200, okResult.Value);
+    }
+
+    [Fact]
+    public void Remove_DeveRetornar404_QuandoNaoEncontrado()
+    {
+        // Arrange
+        var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+        {
+            ReasonPhrase = "Usuário não encontrado"
+        };
+
+        _serviceMock
+            .Setup(s => s.Remove(It.IsAny<int>()))
+            .Returns(response);
+
+        // Act
+        var result = _controller.Remove(1);
+
+        // Assert
+        var notFoundResult = Assert.IsType<ObjectResult>(result);
+        Assert.Equal(404, notFoundResult.StatusCode);
+        Assert.Equal("Usuário não encontrado", notFoundResult.Value);
+    }
+
+    [Fact]
+    public void Remove_DeveRetornar401_QuandoExcecao()
+    {
+        // Arrange
+        _serviceMock
+            .Setup(s => s.Remove(It.IsAny<int>()))
+            .Throws(new Exception());
+
+        // Act
+        var result = _controller.Remove(1);
+
+        // Assert
+        Assert.IsType<UnauthorizedResult>(result);
+    }
+}
---
+++ Test/Usuario.del/UsuarioDelServicesTests.cs
@@ -0,0 +1,62 @@
+﻿using Xunit;
+using Moq;
+using System.Net;
+using Usuario.Application;
+using Usuario.Domain;
+using System.Threading.Tasks;
+
+namespace Test.Usuario.del;
+
+public class UsuarioDelServicesTests
+{
+    private readonly Mock<IUsuarioDel> _usuarioDelMock;
+    private readonly Mock<IUsuarioGet> _usuarioGetMock;
+    private readonly UsuarioDelServices _service;
+
+    public UsuarioDelServicesTests()
+    {
+        _usuarioDelMock = new Mock<IUsuarioDel>();
+        _usuarioGetMock = new Mock<IUsuarioGet>();
+
+        _service = new UsuarioDelServices(
+            _usuarioDelMock.Object,
+            _usuarioGetMock.Object
+        );
+    }
+
+    [Fact]
+    public void Remove_DeveRetornar200_QuandoUsuarioExiste()
+    {
+        // Arrange
+        var usuario = new Usuarios(); // entidade fake
+
+        _usuarioGetMock
+            .Setup(x => x.GetById(It.IsAny<int>()))
+            .ReturnsAsync(usuario);
+
+        // Act
+        var result = _service.Remove(1);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+
+        _usuarioDelMock.Verify(x => x.Remove(usuario), Times.Once);
+    }
+
+    [Fact]
+    public void Remove_DeveRetornar404_QuandoUsuarioNaoExiste()
+    {
+        // Arrange
+        _usuarioGetMock
+            .Setup(x => x.GetById(It.IsAny<int>()))
+            .ReturnsAsync((Usuarios)null);
+
+        // Act
+        var result = _service.Remove(1);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, result.StatusCode);
+
+        _usuarioDelMock.Verify(x => x.Remove(It.IsAny<Usuarios>()), Times.Never);
+    }
+}
